b48cdad95db35b2bb92faa4f9845b916c4186ad3
eya161 <araari.eya@gmail.com>
2023-11-12 14:59:44 +0100

    first commits

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './shared/navbar/navbar.component';
+import { AddUniversiteComponent } from './core/manage-university/add-universite/add-universite.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    AddUniversiteComponent
   ],
   imports: [
     BrowserModule,
