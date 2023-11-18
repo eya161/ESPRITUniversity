74590bc24b04dc4d9824313f5b344e59b9772e76
maGNICHI <manar.gnichi@esprit.tn>
2023-11-19 00:56:44 +0100

    foyer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './shared/navbar/navbar.component';
 import { AddUniversiteComponent } from './core/manage-university/add-universite/add-universite.component';
+import { AddFoyerComponent } from './core/manage-foyer/add-foyer/add-foyer.component';
+import { UpdateFoyerComponent } from './core/manage-foyer/update-foyer/update-foyer.component';
+import { DeleteFoyerComponent } from './core/manage-foyer/delete-foyer/delete-foyer.component';
+import { ListFoyerComponent } from './core/manage-foyer/list-foyer/list-foyer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    AddUniversiteComponent
+    AddUniversiteComponent,
+    AddFoyerComponent,
+    UpdateFoyerComponent,
+    DeleteFoyerComponent,
+    ListFoyerComponent
   ],
   imports: [
     BrowserModule,
